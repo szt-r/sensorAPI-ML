d3427ac0450ff8ffb0cd924368bbb1b91de85cb6
sztxr <sztxr@github.com>
2020-05-26 22:29:05 -0300

    use env variables

--- httpsserver.py
+++ httpsserver.py
@@ -1,7 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
+import settings
 
-httpd = HTTPServer(('192.168.1.79', 4443), SimpleHTTPRequestHandler)
+httpd = HTTPServer((settings.IP_ADDRESS, 4443), SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(
     httpd.socket, certfile='./localhost.pem', server_side=True)
 print('Serving at port', 4443)
